c1c64d20551be6b8e3543a03b11817e76ad2d2b9
Farhan-rashid10 <156300178+Farhan-rashid10@users.noreply.github.com>
2024-05-17 11:01:56 +0300

    index.js

--- second/index.js
+++ second/index.js
@@ -1,8 +1,10 @@
+// calculate the speed of the car
 let speed = 130;
 let speedLimit = 70;
 let aboveSpeed = 5;
 let overSpeed = 1;
-
+// if the speed is less than 70 it's ok 
+// else your licences suspend
 if (speed <= speedLimit) {
     console.log("Ok");
 } else {
